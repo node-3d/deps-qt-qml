e84d8d5b89ce6d612a2f8844715525cb4729ec65
Luis Blanco <luisblanco1337@gmail.com>
2026-07-04 20:43:00 +0400

    Wire oxfmt formatting checks

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import gui, { core } from '@node-3d/deps-qt-gui';
 
 const paths = getPaths(import.meta.dirname);
 
-export { core, gui };
+export { default as gui, core } from '@node-3d/deps-qt-gui';
+
 export const bin = paths.bin;
+
 export const include = paths.include;
 
 const depsQtQml = {
